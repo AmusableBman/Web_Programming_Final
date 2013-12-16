e6eca95a7b82f39df5538b42b2a1f0719e4f640e
Vincent Nguyen <snm-syna@hotmail.com>
2013-12-15 21:55:18 -0500

    Update header1.php
    Adding page title to each page.

--- public_html/header1.php
+++ public_html/header1.php
@@ -1,2 +1,4 @@
-<h1 class="Header"><img id="imglogo" src="images/logo.png" alt="logo"/> </h1>
+
+<h1 class="Header"><img id="imglogo" src="images/logo.png" alt="logo"/></h1>
+<h2><?php echo $page_title;  ?></h2> 
 
