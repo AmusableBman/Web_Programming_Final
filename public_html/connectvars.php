c41f92c15ee2e859b0595c782f082af073a086a1
Vincent Nguyen <snm-syna@hotmail.com>
2013-12-16 17:39:14 -0500

    Update connectvars.php

--- public_html/connectvars.php
+++ public_html/connectvars.php
@@ -1,3 +1,9 @@
+<!--
+File Name : connectvars.php
+Author : Brandon Hewlett & Vincent Nguyen
+Website Name : Zepic
+File Description : The page provides the credentials to connect to the database.
+-->
 <?php
   define('DB_HOST', 'webdesign4.georgianc.on.ca');
   define('DB_USER', 'db200210636');
