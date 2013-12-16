6e821199c080b297f2a283f284e23cf2cccb0f43
Vincent Nguyen <snm-syna@hotmail.com>
2013-12-16 17:46:55 -0500

    Update index2.php

--- public_html/index2.php
+++ public_html/index2.php
@@ -16,18 +16,23 @@
     <body>
 
         <?php
+       //changes the header name
        $page_title = 'Home Page';
        require_once('header1.php')
+       
+       //starts the session
        require_once('initializesession.php');
        require_once('connectvars.php');
+       
+       //includes the navigation bar
        require_once('navigation.php');
        
        $idtodelete= $_GET['idtodelete'];
 
-  // If the user isn't logged in, try to log them in
+  // If user isnt on, try logging them on.
   if (!isset($_SESSION['user_id'])) {
     if (isset($_POST['submit'])) {
-      // Connect to the database
+      // Connect database
       $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
       
       if(!empty($idtodelete))
@@ -37,32 +42,39 @@
             echo "<p>".$deletequery."</p>";
         }
 
-      // Grab the user-entered log-in data
+      // Grab the data entered
       $user_username = mysqli_real_escape_string($dbc, trim($_POST['username']));
       $user_password = mysqli_real_escape_string($dbc, trim($_POST['password']));
 
       if (!empty($user_username) && !empty($user_password)) {
-        // Look up the username and password in the database
+        
+        // Search for username and password in database
         $query = "SELECT user_id, username FROM Zepic_user WHERE username = '$user_username' AND password = SHA('$user_password')";
         $data = mysqli_query($dbc, $query);
 
         if (mysqli_num_rows($data) == 1) {
-          // The log-in is OK so set the user ID and username session vars (and cookies), and redirect to the home page
+          // Validating sucessful let the user ID and username, session vars, and cookies, redirect to main page.
           $row = mysqli_fetch_array($data);
           $_SESSION['user_id'] = $row['user_id'];
           $_SESSION['username'] = $row['username'];
-          setcookie('user_id', $row['user_id'], time() + (60 * 60 * 24 * 30));    // expires in 30 days
-          setcookie('username', $row['username'], time() + (60 * 60 * 24 * 30));  // expires in 30 days
+          setcookie('user_id', $row['user_id'], time() + (60 * 60 * 24 * 30));    // exp in 30 days
+          setcookie('username', $row['username'], time() + (60 * 60 * 24 * 30));  // exp in 30 days
           $home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index1.php';
           header('Location: ' . $home_url);
         }
+        
         else {
-          // The username/password are incorrect so set an error message
+        
+          //  Data incorrect, provide error.
           $error_msg = 'Sorry, you must enter a valid username and password to log in.';
-        }
+      
+       }
+    
       }
+      
       else {
-        // The username/password weren't entered so set an error message
+       
+        //  Data incorrect, provide error.
         $error_msg = 'Sorry, you must enter your username and password to log in.';
       }
     }
